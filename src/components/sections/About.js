6223316dec5ddf6a1260cf3bb56d169c2507d0cd
norindes <norindes@gmail.com>
2023-07-10 20:46:54 -0600

    redesign

---
+++ src/components/sections/About.js
@@ -0,0 +1,176 @@
+import React, { lazy, Suspense } from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+// import Carousel from '../Carousel'
+import Button from '../Button'
+import {dark} from '../../styles/Themes';
+import Loading from '../Loading';
+
+
+const Carousel = lazy(() => import("../Carousel"));
+
+const Section = styled.section`
+min-height: 100vh;
+width: 100%;
+background-color: ${props => props.theme.text};
+display: flex;
+justify-content: center;
+align-items: center;
+position: relative;
+overflow: hidden;
+
+`
+const Container = styled.div`
+width: 75%;
+margin: 0 auto;
+/* background-color: lightblue; */
+
+display: flex;
+justify-content: center;
+align-items: center;
+@media (max-width: 70em){
+  width: 85%;
+}
+
+@media (max-width: 64em){
+  width: 100%;
+  flex-direction: column;
+
+  &>*:last-child{
+    width: 80%;
+  }
+}
+@media (max-width: 40em){
+  
+
+  &>*:last-child{
+    width: 90%;
+  }
+}
+`
+const Box = styled.div`
+width: 50%;
+height: 100%;
+min-height: 60vh;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+@media (max-width: 40em){
+  min-height: 50vh;
+}
+`
+ 
+const Title = styled.h2`
+  font-size: ${(props) => props.theme.fontxxl};
+  text-transform: capitalize;
+  color: ${(props) => props.theme.body};
+  align-self: flex-start;
+  width: 80%;
+margin: 0 auto;
+
+@media (max-width: 64em){
+  width: 100%;
+  text-align:center;
+}
+@media (max-width: 40em){
+  font-size: ${(props) => props.theme.fontxl};
+
+}
+@media (max-width: 30em){
+  font-size: ${(props) => props.theme.fontlg};
+
+}
+`
+const SubText = styled.p`
+  font-size: ${(props) => props.theme.fontlg};
+  color: ${(props) => props.theme.body};
+  align-self: flex-start;
+  width: 80%;
+margin: 1rem auto;
+font-weight:400;
+@media (max-width: 64em){
+  width: 100%;
+  text-align:center;
+  font-size: ${(props) => props.theme.fontmd};
+
+}
+@media (max-width: 40em){
+  font-size: ${(props) => props.theme.fontmd};
+
+}
+@media (max-width: 30em){
+  font-size: ${(props) => props.theme.fontsm};
+
+}
+
+`
+const SubTextLight = styled.p`
+  font-size: ${(props) => props.theme.fontmd};
+  color: ${(props) => `rgba(${props.theme.bodyRgba},0.6)`};
+  align-self: flex-start;
+  width: 80%;
+margin: 1rem auto;
+font-weight:400;
+
+@media (max-width: 64em){
+  width: 100%;
+  text-align:center;
+  font-size: ${(props) => props.theme.fontsm};
+
+}
+@media (max-width: 40em){
+  font-size: ${(props) => props.theme.fontsm};
+
+}
+@media (max-width: 30em){
+  font-size: ${(props) => props.theme.fontxs};
+
+}
+
+`
+const ButtonContainer = styled.div`
+ width: 80%;
+ margin: 1rem auto;
+ display: flex;
+  align-self: flex-start;
+
+  @media (max-width: 64em){
+width: 100%;
+
+button{
+  margin: 0 auto;
+}
+}
+
+`
+
+const About = () => {
+  return (
+    <Section id="about">
+      <Container>
+        <Box> 
+        <Suspense fallback={<Loading />}>
+          <Carousel /> </Suspense> </Box>
+        <Box> 
+        <Title>
+        La coleccion. Weirdos Club.
+        </Title>
+        <SubText>
+        Pixel Homies es una coleccion de 1000 personajes coleccionables inspirados en la cultura mexicana con escenas clasicas de Guadalajara, Jalisco. Cada Pixel Homie fue generado algoritmicamente y cuentan con mas de 200 distintas variantes las cuales fueron hechas a mano, pixel por pixel. El precio por mint es de 0.015 ETH y seran lanzados en la red de Ethereum.
+        </SubText>
+        <SubTextLight>
+        Al comprar un Pixel Homie estas comprando un pedazo de nuestra cultura mexicana y perteneceras a nuestra comunidad en la cual nos enorgullece celebrar nuestras costumbres.<br /><br />Para mantenerte al tanto sobre nuestro mint inicial, whitelist y mas unete a nuestra comunidad en Discord, siguenos en Twitter e Instagram presionando los iconos en el pie de pagina. 
+        </SubTextLight>
+        <ButtonContainer>
+        <ThemeProvider theme={dark}>
+        <Button text="Unete al Discord" link="https://discord.com/invite/8Q5gsJ4py6" />
+        </ThemeProvider>
+        </ButtonContainer>
+         </Box>
+      </Container>
+    </Section>
+  )
+}
+
+export default About
